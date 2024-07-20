939a1ab339d41cc1ad9aafced116eb8517254b45
Oliver Edge <BearPublishing1@gmail.com>
2024-07-20 11:27:29 +0100

    modified:   .vs/Bear Kart Launcher/DesignTimeBuild/.dtbcache.v2 	modified:   .vs/Bear Kart Launcher/v17/.suo 	modified:   .vs/Bear Kart Launcher/v17/DocumentLayout.json 	modified:   .vs/ProjectEvaluation/bear kart launcher.metadata.v8.bin 	modified:   .vs/ProjectEvaluation/bear kart launcher.projects.v8.bin 	deleted:    Bear Kart Launcher/bin/Release/net8.0-windows/Bear Kart Launcher.deps.json 	deleted:    Bear Kart Launcher/bin/Release/net8.0-windows/Bear Kart Launcher.dll 	deleted:    Bear Kart Launcher/bin/Release/net8.0-windows/Bear Kart Launcher.exe 	deleted:    Bear Kart Launcher/bin/Release/net8.0-windows/Bear Kart Launcher.pdb 	deleted:    Bear Kart Launcher/bin/Release/net8.0-windows/Bear Kart Launcher.runtimeconfig.json 	deleted:    Bear Kart Launcher/bin/Release/net8.0-windows/publish/Bear Kart Launcher.deps.json 	deleted:    Bear Kart Launcher/bin/Release/net8.0-windows/publish/Bear Kart Launcher.dll 	deleted:    Bear Kart Launcher/bin/Release/net8.0-windows/publish/Bear Kart Launcher.exe 	deleted:    Bear Kart Launcher/bin/Release/net8.0-windows/publish/Bear Kart Launcher.pdb 	deleted:    Bear Kart Launcher/bin/Release/net8.0-windows/publish/Bear Kart Launcher.runtimeconfig.json 	deleted:    Bear Kart Launcher/bin/Release/net8.0-windows/publish/bkl 1.2.zip 	modified:   Bear Kart Launcher/obj/Debug/net8.0-windows/Bear Kart Launcher.AssemblyInfo.cs 	modified:   Bear Kart Launcher/obj/Debug/net8.0-windows/Bear Kart Launcher.AssemblyInfoInputs.cache 	new file:   README.md

--- Bear Kart Launcher/obj/Debug/net8.0-windows/Bear Kart Launcher.AssemblyInfo.cs
+++ Bear Kart Launcher/obj/Debug/net8.0-windows/Bear Kart Launcher.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bear Kart Launcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94bda373cbd395b02008065281bae1272f3c8352")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bear Kart Launcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bear Kart Launcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,7 @@
+This is a launcher for all 3 Bear Kart versions with support for custom repos
+
+# Building
+To build from source open `Bear Kart Launcher.sln` in Visual Studio
+Right click the solution explorer and click _Publish_ and publish the solution
+You can find the output in `./Bear Kart Launcher/bin/Release/net8.0-windows/publish/*`
+Alternatively you can download the latest release from the releases
