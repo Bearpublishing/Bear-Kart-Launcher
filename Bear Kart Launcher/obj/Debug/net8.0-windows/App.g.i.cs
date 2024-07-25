0f2caa1e4ed1b6aecaff7cd1631dfa7594d92d46
Oliver Edge <BearPublishing1@gmail.com>
2024-07-25 10:29:00 +0100

    modified:   .vs/Bear Kart Launcher/DesignTimeBuild/.dtbcache.v2 	modified:   .vs/Bear Kart Launcher/v17/.suo 	modified:   .vs/Bear Kart Launcher/v17/DocumentLayout.json 	modified:   .vs/ProjectEvaluation/bear kart launcher.metadata.v8.bin 	modified:   .vs/ProjectEvaluation/bear kart launcher.projects.v8.bin 	modified:   .vs/ProjectEvaluation/bear kart launcher.strings.v8.bin 	modified:   Bear Kart Launcher/obj/Bear Kart Launcher.csproj.nuget.dgspec.json 	modified:   Bear Kart Launcher/obj/Debug/net8.0-windows/App.g.i.cs 	modified:   Bear Kart Launcher/obj/Debug/net8.0-windows/Bear Kart Launcher.AssemblyInfo.cs 	modified:   Bear Kart Launcher/obj/Debug/net8.0-windows/Bear Kart Launcher.AssemblyInfoInputs.cache 	modified:   Bear Kart Launcher/obj/Debug/net8.0-windows/Bear Kart Launcher_MarkupCompile.i.cache 	new file:   Bear Kart Launcher/obj/Debug/net8.0-windows/Bear Kart Launcher_MarkupCompile.i.lref 	modified:   Bear Kart Launcher/obj/Debug/net8.0-windows/MainWindow.g.i.cs 	modified:   Bear Kart Launcher/obj/project.assets.json 	modified:   Bear Kart Launcher/obj/project.nuget.cache

--- Bear Kart Launcher/obj/Debug/net8.0-windows/App.g.i.cs
+++ Bear Kart Launcher/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Bear_Kart_Launcher {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.7.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Bear_Kart_Launcher {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.7.0")]
         public static void Main() {
             Bear_Kart_Launcher.App app = new Bear_Kart_Launcher.App();
             app.InitializeComponent();
--- Bear Kart Launcher/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ Bear Kart Launcher/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -127,13 +127,13 @@ namespace Bear_Kart_Launcher {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.7.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Bear Kart Launcher;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Bear Kart Launcher;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -143,7 +143,7 @@ namespace Bear_Kart_Launcher {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.7.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
